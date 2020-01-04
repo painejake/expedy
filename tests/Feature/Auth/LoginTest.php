8de2bd202fd77fd975d62ecbb0206320a245d8b5
Jake Paine <jakerpaine@gmail.com>
2020-01-04 12:00:57 +0000

    Finish adding the basic login unit tests

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -11,6 +11,12 @@ class LoginTest extends TestCase
 {
     use RefreshDatabase;
 
+    /**
+     * Test the login form is working.
+     *
+     * @return void
+     * @since 1.0.0
+     */
     public function testUserCanViewLoginForm()
     {
         $response = $this->get('/login');
@@ -20,6 +26,13 @@ class LoginTest extends TestCase
         $response->assertViewIs('auth.login');
     }
 
+    /**
+     * Test the user cannot view the login form after they have
+     * authenticated.
+     *
+     * @return void
+     * @since 1.0.0
+     */
     public function testUserCannotViewLoginFormWhenAuthenticated()
     {
         $user = factory(User::class)->make();
@@ -29,6 +42,12 @@ class LoginTest extends TestCase
         $response->assertRedirect('/home');
     }
 
+    /**
+     * Test the user can login when using the correct credentials.
+     *
+     * @return void
+     * @since 1.0.0
+     */
     public function testUserCanLoginWithCorrectCredentials()
     {
         $user = factory(User::class)->create([
@@ -44,4 +63,59 @@ class LoginTest extends TestCase
 
         $this->assertAuthenticatedAs($user);
     }
+
+    /**
+     * Test the user cannot login with incorrect credentials.
+     *
+     * @return void
+     * @since 1.0.0
+     */
+    public function testUserCannotLoginWithIncorrectPassword()
+    {
+        $user = factory(User::class)->create([
+            'password' => bcrypt('laravel'),
+        ]);
+        
+        $response = $this->from('/login')->post('/login', [
+            'email' => $user->email,
+            'password' => 'invalid-password',
+        ]);
+        
+        $response->assertRedirect('/login');
+        $response->assertSessionHasErrors('email');
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    /**
+     * Test the remember me cookie functionality.
+     *
+     * @return void
+     * @since 1.0.0
+     */
+    public function testRememberMeFunctionality()
+    {
+        $user = factory(User::class)->create([
+            'id' => random_int(1, 100),
+            'password' => bcrypt($password = 'laravel'),
+        ]);
+        
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+            'remember' => 'on',
+        ]);
+        
+        $response->assertRedirect('/home');
+
+        $response->assertCookie(\Auth::guard()->getRecallerName(), vsprintf('%s|%s|%s', [
+            $user->id,
+            $user->getRememberToken(),
+            $user->password,
+        ]));
+
+        $this->assertAuthenticatedAs($user);
+    }
+
 }
